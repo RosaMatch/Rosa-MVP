7ef92dc4f911c54f2cbc6d480c59c9f45fb76332
Jeffrey Oyuela <jeffreysmac@Jeffreys-MacBook-Pro.local>
2025-01-04 13:45:43 -0500

    Changed Icon and Created Constants.dart

---
+++ app/lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color pageBackgroundColor = Color.fromRGBO(238, 242, 232, 1);
+
+ThemeData appTheme = ThemeData(
+  scaffoldBackgroundColor: pageBackgroundColor,
+);
