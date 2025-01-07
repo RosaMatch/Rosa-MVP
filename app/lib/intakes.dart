c047dc2b2e8856f506c5fd753102890fafb21508
AndrewPeralta11 <ap238@buffalo.edu>
2025-01-07 14:48:18 -0500

    Found a router library that has worked to transition between the homepage and the intakes page. Documentation on the library can be found here: https://pub.dev/packages/go_router.

---
+++ app/lib/intakes.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'constants.dart';
+
+class Intakes extends StatelessWidget {
+  const Intakes({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            AppBar(
+              title: Row(
+                children: [
+                  Image.asset(
+                    'assets/icon/rosa_icon.png',
+                    width: 74,
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(left: 268.0),
+                    child: Icon(Icons.settings,
+                      color: Colors.white,
+                      size: 64,
+                      ),
+                  )
+                  
+                ],
+              ),
+              toolbarHeight: 84,
+              backgroundColor: rosaRedColor,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                context.go('/');
+              },
+              child: const Text('Go to Home'),
+            ),
+          ]
+        )
+      )
+    );
+  }
+}
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'constants.dart';
 import 'homepage.dart';
+import 'intakes.dart';
 
 void main() {
   runApp(const MainApp());
 }
+
+final GoRouter _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const Home(),
+    ),
+    GoRoute(
+      path: '/intakes',
+      builder: (context, state) => const Intakes(),
+    ),
+  ],
+);
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: _router,
       theme: appTheme,
-      home: Home(),
     );
   }
 }
