12b1b8cf31c43db5ed43382d8bfa562041931e25
Jeffrey Oyuela <jeffreysmac@Jeffreys-MacBook-Pro.local>
2025-01-07 21:20:36 -0500

    Added BottomNavigationBar

--- app/lib/constants.dart
+++ app/lib/constants.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 const Color pageBackgroundColor = Color.fromRGBO(238, 242, 232, 1);
 const Color rosaRedColor = Color.fromRGBO(82, 25, 39, 1);
+const Color navigationBarColor = Color.fromRGBO(227, 227, 210, 1);
 
 ThemeData appTheme = ThemeData(
   scaffoldBackgroundColor: pageBackgroundColor,
