38da178c611b8691cf3c938cf7d75459b258ea01
Jeffrey Oyuela <jeffreysmac@Jeffreys-MacBook-Pro.local>
2025-01-21 14:44:08 -0500

    Changed Button colors for intake form pages 2/4 and 3/4

--- app/lib/constants.dart
+++ app/lib/constants.dart
@@ -3,12 +3,13 @@ import 'package:google_fonts/google_fonts.dart';
 
 //COLORS
 const Color backgroundColorCream1 = Color.fromRGBO(238, 242, 232, 1);
+const Color backgroundColorCream2 = Color.fromRGBO(208, 198, 188, 1);
+const Color backgroundColorCream3 = Color.fromRGBO(243,252, 250, 1);
 const Color rosaRedColor = Color.fromRGBO(82, 25, 39, 1);
 const Color rosaRedSplashColor = Color.fromRGBO(82, 25, 39, 0.3);
 const Color textFormBorderColor = Color.fromRGBO(227, 227, 210, 1);
 const Color inactiveRedColor = Color.fromRGBO(176, 146, 156, 1);
 const Color activeRedColor = Color.fromRGBO(108, 86, 92, 1);
-const Color backgroundColorCream2 = Color.fromRGBO(208, 198, 188, 1);
 const Color dividerColor = Color.fromRGBO(176, 146, 156, 1);
 
 //const Size loginButtonSize = Size(300, 30);
